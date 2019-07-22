3e600e19e13c2c3925963fe06ac910a58178fdbc
moorejr5 <james.r.moore@vanderbilt.edu>
2019-07-22 12:13:01 -0500

    Testing out the user roles.

--- js/userRoles.js
+++ js/userRoles.js
@@ -386,6 +386,7 @@ $(function() {
 				}
                 var role_id = selectedRole.projects[pid]['role']
                 var group_id = selectedRole.projects[pid]['dag']
+				console.log('Checking for '+pid);
                 if (UserRoles.projects[pid]) {
                 	console.log('User role set for '+pid);
                     UserRoles.addProjectRow(pid, role_id, group_id)
